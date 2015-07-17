e2c301e57e774efa88fe1eec7a90aab71e85a97c
atton <e115763@gmail.com>
2015-07-17 10:34:38 +0900

    Add and fix x86_64 elf utils from altkatz/gcc_cross_compilers

---
+++ x64-elf-binutils.rb
@@ -0,0 +1,26 @@
+require 'formula'
+
+class X64ElfBinutils < Formula
+  homepage 'http://gcc.gnu.org'
+  url 'http://ftp.gnu.org/gnu/binutils/binutils-2.23.tar.gz'
+  sha1 '470c388c97ac8d216de33fa397d7be9f96c3fe04'
+
+  depends_on 'gcc49'
+
+  def install
+    ENV['CC'] = '/usr/local/bin/gcc-4.9'
+    ENV['CXX'] = '/usr/local/bin/g++-4.9'
+    ENV['CPP'] = '/usr/local/bin/cpp-4.9'
+    ENV['LD'] = '/usr/local/bin/gcc-4.9'
+
+    mkdir 'build' do
+      system '../configure', '--disable-nls', '--target=x86_64-elf','--disable-werror',
+                             '--enable-gold=yes',
+                             "--prefix=#{prefix}"
+      system 'make all'
+      system 'make install'
+      FileUtils.mv lib, libexec
+    end
+  end
+
+end
