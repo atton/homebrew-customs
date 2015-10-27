3462b4393dbb416fb144e4256475162c965c19c2
atton <e115763@gmail.com>
2015-10-27 15:02:48 +0900

    Add patch for tmux 2.1

--- tmux.rb
+++ tmux.rb
@@ -17,9 +17,14 @@ class Tmux < Formula
   depends_on "pkg-config" => :build
   depends_on "libevent"
 
-  patch do
+  patch :p1 do
     url 'https://gist.githubusercontent.com/waltarix/1399751/raw/e60e879335bf3b91fef4592b194cc524bcb95388/tmux-ambiguous-width-cjk.patch'
     sha1 'cc138b1893b15c0bd57cf3956c6a3e8120abc7d0'
+  end
+
+  patch :p1 do
+    url "https://gist.githubusercontent.com/waltarix/1399751/raw/f1b8fba43f80792661cd468584e8c13438e7c3cd/tmux-pane-border-ascii.patch"
+    sha256 "f2838066b2765a19c0d4421beb885302b9e4dba94cb8b9419a58a51da5a781cb"
   end
 
   def install
