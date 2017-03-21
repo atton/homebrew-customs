88022f181cddcdb01233e760a4473acf18b0cc76
atton <e115763@gmail.com>
2017-03-21 15:27:24 +0900

    Update tmux to 2.3

--- tmux.rb
+++ tmux.rb
@@ -1,49 +1,44 @@
-require 'formula'
-
 class Tmux < Formula
   desc "Terminal multiplexer"
   homepage "https://tmux.github.io/"
-  url "https://github.com/tmux/tmux/releases/download/2.1/tmux-2.1.tar.gz"
-  sha256 "31564e7bf4bcef2defb3cb34b9e596bd43a3937cad9e5438701a81a5a9af6176"
+  head "https://github.com/atton-/tmux", using: :git, branch: 'border-ascii'
+  version '2.3'
 
-  head do
-    url "https://github.com/tmux/tmux.git"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool"  => :build
-  end
-
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
   depends_on "pkg-config" => :build
   depends_on "libevent"
+  depends_on "utf8proc" => :optional
 
-  patch :p1 do
-    url 'https://gist.githubusercontent.com/waltarix/1399751/raw/e60e879335bf3b91fef4592b194cc524bcb95388/tmux-ambiguous-width-cjk.patch'
-    sha256 'b77fa3de2cd43f1fe0c0423576b38e363d387343e8d0ed8a2702e67068d64fdf'
-  end
-
-  patch :p1 do
-    url "https://gist.githubusercontent.com/waltarix/1399751/raw/f1b8fba43f80792661cd468584e8c13438e7c3cd/tmux-pane-border-ascii.patch"
-    sha256 "f2838066b2765a19c0d4421beb885302b9e4dba94cb8b9419a58a51da5a781cb"
+  resource "completion" do
+    url "https://raw.githubusercontent.com/imomaliev/tmux-bash-completion/homebrew_1.0.0/completions/tmux"
+    sha256 "05e79fc1ecb27637dc9d6a52c315b8f207cf010cdcee9928805525076c9020ae"
   end
 
   def install
     system "sh", "autogen.sh" if build.head?
 
+    args = %W[
+      --disable-Dependency-tracking
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}
+    ]
+
+    args << "--enable-utf8proc"
+
     ENV.append "LDFLAGS", "-lresolv"
-    system "./configure", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}",
-                          "--sysconfdir=#{etc}"
+    system "./configure", *args
 
     system "make", "install"
 
-    bash_completion.install "examples/bash_completion_tmux.sh" => "tmux"
-    pkgshare.install "examples"
+    pkgshare.install "example_tmux.conf"
+    bash_completion.install resource("completion")
   end
 
   def caveats; <<-EOS.undent
-    Example configurations have been installed to:
-      #{opt_pkgshare}/examples
+    Example configuration has been installed to:
+      #{opt_pkgshare}
     EOS
   end
 
