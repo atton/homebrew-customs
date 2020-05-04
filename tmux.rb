5759efaa6af1181519e7777e67785513bdf692a2
atton <e115763@gmail.com>
2020-05-05 07:32:50 +0900

    Update tmux to 3.1b-border-ascii

--- tmux.rb
+++ tmux.rb
@@ -1,20 +1,26 @@
 class Tmux < Formula
   desc "Terminal multiplexer"
-  homepage "https://tmux.github.io/"
-  head "https://github.com/atton/tmux", using: :git, branch: '3.0a-border-ascii'
-  version '3.0a'
+  homepage "https://tmux.github.io"
+  head "https://github.com/atton/tmux", using: :git, branch: '3.1b-border-ascii'
+  version '3.1b'
 
   depends_on "autoconf"
   depends_on "automake"
-  depends_on "libtool"
-  depends_on "pkg-config"
   depends_on "libevent"
+  depends_on "libtool"
   depends_on "ncurses"
+  depends_on "pkg-config"
+
+  resource "completion" do
+    url "https://raw.githubusercontent.com/imomaliev/tmux-bash-completion/homebrew_1.0.0/completions/tmux"
+    sha256 "05e79fc1ecb27637dc9d6a52c315b8f207cf010cdcee9928805525076c9020ae"
+  end
 
   def install
     system "sh", "autogen.sh"
 
     args = %W[
+      --disable-dependency-tracking
       --prefix=#{prefix}
       --sysconfdir=#{etc}
     ]
@@ -23,6 +29,7 @@ class Tmux < Formula
     system "./configure", *args
 
     system "make", "install"
+    bash_completion.install resource("completion")
   end
 
   test do
