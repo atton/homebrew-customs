248e9dfc64372d2524cc64f1d8598adab11d6c05
atton <e115763@gmail.com>
2020-07-06 11:12:16 +0900

    Remove tmux completion script for bash

--- tmux.rb
+++ tmux.rb
@@ -11,11 +11,6 @@ class Tmux < Formula
   depends_on "ncurses"
   depends_on "pkg-config"
 
-  resource "completion" do
-    url "https://raw.githubusercontent.com/imomaliev/tmux-bash-completion/homebrew_1.0.0/completions/tmux"
-    sha256 "05e79fc1ecb27637dc9d6a52c315b8f207cf010cdcee9928805525076c9020ae"
-  end
-
   def install
     system "sh", "autogen.sh"
 
@@ -29,7 +24,6 @@ class Tmux < Formula
     system "./configure", *args
 
     system "make", "install"
-    bash_completion.install resource("completion")
   end
 
   test do
